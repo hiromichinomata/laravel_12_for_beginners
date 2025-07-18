7f42a94182e1e16bbff922ca623f3c567b6cf4b7
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-18 21:43:35 +0900

    add: IsAdminMiddleware

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Middleware\IsAdminMiddleware;
 
 Route::get('/welcome', function () {
     return view('welcome');
@@ -15,16 +16,17 @@ Route::view('about', 'about')->name('about');
 Route::view('article', 'article')->name('article');
 Route::get('posts/{post}', [PostController::class, 'show'])
     ->name('post.show');
-Route::resource('categories', CategoryController::class);
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['verified'])->name('dashboard');
+
+    Route::resource('categories', CategoryController::class)->middleware(IsAdminMiddleware::class);
 });
 
 require __DIR__.'/auth.php';
