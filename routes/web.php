fabb96462678c3a3027fd7c851a7db54f05e7307
Hiromichi NOMATA <git@hiromichinomata.com>
2025-07-18 21:00:53 +0900

    add: breeze

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
-
 Route::view('article', 'article')->name('article');
-
 Route::get('posts/{post}', [PostController::class, 'show'])
     ->name('post.show');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
